2f26811fecb408b334b0998c15157094e3735175
felipe <andresfelipe310659@gmail.com>
2018-08-21 20:31:41 -0500

    practica

--- academico.py
+++ academico.py
@@ -1,11 +1,18 @@
-Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:06:47) [MSC v.1914 32 bit (Intel)] on win32
-Type "copyright", "credits" or "license()" for more information.
->>> 
-================ RESTART: C:/felipe/python_class/academico.py ================
-SISTEMA DE INFORMACION ACADEMICO - PROMEDIO DE EDADES
-el estudiante Juan Carlos Arteaga tiene 15 años
-el estudiante Angela Marta Lopez tiene 18 años
-el estudiante Roberto Andres Jurado tiene 22 años
-el estudiante Monica Maribel Hurtado tiene 19 años
-el promedio de edades es:  18.5
->>> 
+sumaedades=0
+promedio=0
+edad1=15
+persona1= "Juan Carlos Arteaga"
+edad2=18
+persona2= "Angela Marta Lopez"
+edad3=22
+persona3= "Roberto Andres Jurado"
+edad4=19
+persona4= "Monica Maribel Hurtado"
+print ("SISTEMA DE INFORMACION ACADEMICO - PROMEDIO DE EDADES")
+sumaedades=edad1+edad2+edad3+edad4
+print ("el estudiante",persona1,"tiene",edad1,"años")
+print ("el estudiante",persona2,"tiene",edad2,"años")
+print ("el estudiante",persona3,"tiene",edad3,"años")
+print ("el estudiante",persona4,"tiene",edad4,"años")
+promedio=sumaedades/4
+print ("el promedio de edades es: ",promedio)
