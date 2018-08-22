ea542b57b7f1230cd5ed32cf741c1ffc83cc2511
felipe <andresfelipe310659@gmail.com>
2018-08-21 20:26:51 -0500

    practica

---
+++ academico.py
@@ -0,0 +1,11 @@
+Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:06:47) [MSC v.1914 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+================ RESTART: C:/felipe/python_class/academico.py ================
+SISTEMA DE INFORMACION ACADEMICO - PROMEDIO DE EDADES
+el estudiante Juan Carlos Arteaga tiene 15 años
+el estudiante Angela Marta Lopez tiene 18 años
+el estudiante Roberto Andres Jurado tiene 22 años
+el estudiante Monica Maribel Hurtado tiene 19 años
+el promedio de edades es:  18.5
+>>> 
