d87a78724ebced168ca93f6a6f1d6a5503a185da
felipe <andresfelipe310659@gmail.com>
2018-08-27 21:46:49 -0500

    practica2

---
+++ numero_mayor.py
@@ -0,0 +1,10 @@
+
+n1=0
+n2=0
+n1=input("ingrese primer numero:")
+n2=input("ingrese segundo nemero:")
+print("***************************")
+if n1>n2:
+		print ("el numero mayor es:",n1)
+else:
+	print("el numero mayor es",n2)		
