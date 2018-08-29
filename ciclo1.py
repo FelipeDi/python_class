4af953cad103ee85cd6e2785c26e255068e79977
andresfelipe310659@gmail.com <andresfelipe310659@gmail.com>
2018-08-29 14:54:16 -0500

    OK

---
+++ ciclo1.py
@@ -0,0 +1,24 @@
+#numeros manuales 
+print("1")
+print("2")
+print("3")
+print("4")
+print("5")
+print("6")
+print("7")
+print("8")
+print("9")
+print("10")
+#numeros con ciclo while 
+num=0
+while num<=10:
+	print(num)
+	num=num+1
+#tabla del 5
+#tabla=5
+tabla=int(input("digite la tabla a multiplicar: "))
+num=1
+while num<=10:
+	print(tabla, "*", num, "=", tabla *num)
+	num=num + 1
+
