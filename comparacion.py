1893cd181d454ef8f50bbad51bb94d3d70843ba0
felipe <andresfelipe310659@gmail.com>
2018-08-28 19:54:22 -0500

    prac2

--- comparacion.py
+++ comparacion.py
@@ -1,7 +1,7 @@
 n1=0
 n2=0
-n1=input("ingrese primer numero:")
-n2=input("ingrese segundo numero")
+n1=int(input("ingrese primer numero:"))
+n2=int(input("ingrese segundo numero"))
 if n1>n2:
 	print("el numero mayor es:",n1)
 else:
--- numero_mayor.py
+++ numero_mayor.py
@@ -1,8 +1,8 @@
 
 n1=0
 n2=0
-n1=input("ingrese primer numero:")
-n2=input("ingrese segundo nemero:")
+n1=int(input("ingrese primer numero:"))
+n2=int(input("ingrese segundo nemero:"))
 print("***************************")
 if n1>n2:
 		print ("el numero mayor es:",n1)
