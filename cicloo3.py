2a6deab141ba1dfda7d50fd7ae54931f0742f362
felipe <andresfelipe310659@gmail.com>
2018-09-04 23:18:43 -0500

    listo

---
+++ cicloo3.py
@@ -0,0 +1,8 @@
+#ciclo tres 
+rango =1
+rango1 = int(input("ingrese rango de la tabla a desarrollar"))
+tabla = int(input("ingrese numero de la tabla a deazrrollar"))
+while rango <= rango1 :
+	print (tabla, "*", rango, "=" ,tabla*rango)
+	rango = rango+1
+print("************************************************")
