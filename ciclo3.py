278e7ed6528dfe0d80af98d542ea901ef6575844
felipe <andresfelipe310659@gmail.com>
2018-09-04 21:39:36 -0500

    ciclo 3

---
+++ ciclo3.py
@@ -0,0 +1,9 @@
+tabla_i = int(input("ingrese desde que tabla se va a resolver: "))
+tabla_max = int(input("ingrese el fin del rango: "))
+while tabla_i <= tabla_max :
+	multip = 1
+	while multip <= 10 :
+		print (tabla_i ,"*", multip, "=", tabla_i * multip)
+		multip = multip + 1
+	tabla_i = tabla_i + 1
+    
